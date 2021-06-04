31ff3b289807ff6a7f7a0ae5cc08f9b2bed2cd0c
JankeeMunkey <jankeemunkey@gmail.com>
2021-06-03 20:41:16 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIAXZHQ6I2H2W7WO5H3"
-  secret_key = "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"
+  region     = ""
+  access_key = ""
+  secret_key = ""
 }
 
