e8b71be9bb9ac5a9404df88c90db615c89874531
SkylarHoughtonGithub <skylarhoughtongithub@gmail.com>
2023-01-17 20:43:50 -0600

    Added Task6-8 and ssh key

--- Ansible/RHCE8/Answers/Task2/bootstrap_managednodes.sh
+++ Ansible/RHCE8/Answers/Task2/bootstrap_managednodes.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+#Example pub key to use
+# ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGUmcJVUSe31lESr7gUgolioS8hHaB8SmkFbv6/mXSC47rE91llOTp/ZzgFGg18dG8txawi1IpLyZ6AfUR+UdRnqgEWMn1OhTX6yynUHj7uVQDl1oF8ozkbnYNNUmH0f04YJFAKk4AQkWTcYapjbsRTr2R9QJ4TH8r7dcuEBdCWP+S3Ww== shoughtsky@control.ansi.example.com
+
 # To run on managed nodes
 sudo bash -c "useradd shoughtsky; usermod -aG wheel shoughtsky ; mkdir -p /home/shoughtsky/.ssh ; echo "shoughtsky" | passwd shoughtsky --stdin"
 sudo bash -c 'chown shoughtsky. /home/shoughtsky/.ssh ; chmod 700 /home/shoughtsky/.ssh'
