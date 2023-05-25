da0c862b3e4dcc597bad2d8c35e9c8eb393ba2e7
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2023-05-24 23:17:35 -0500

    added gather_facts role

--- Terraform/development/gcp/gcp-compute-base/modules/gcp_vms/scripts/bootstrap-ansible-control.sh
+++ Terraform/development/gcp/gcp-compute-base/modules/gcp_vms/scripts/bootstrap-ansible-control.sh
@@ -4,33 +4,37 @@ sudo adduser ansible
 sudo usermod -aG wheel ansible
 sudo passwd ansible
 sudo bash -c "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd"
+sudo systemctl restart sshd
 
 sudo su - ansible
 
-sudo mkdir -p /etc/ansible/inventory
-sudo mkdir -p /etc/ansible/playbooks
-sudo mkdir -p /etc/ansible/roles
-sudo ansible-config init >> /etc/ansible/ansible.cfg
-
-sudo sed -i 's/^inventory*/inventory=/etc/ansible/inventory/hosts' /etc/ansible/ansible.cfg
-sudo sed -i 's/^forks*/forks=50' /etc/ansible/ansible.cfg
-sudo sed -i 's/^forks*/forks=50' /etc/ansible/ansible.cfg
-sudo sed -i 's/^remote_user*/remote_user=ansible' /etc/ansible/ansible.cfg
-sudo sed -i 's/^become_user*/become_user=ansible' /etc/ansible/ansible.cfg
-
-sudo touch /etc/ansible/inventory/hosts
-sudo echo "[control]" >> /etc/ansible/inventory/hosts
-sudo echo "localhost ansible_host=localhost ansible_connection=local" >> /etc/ansible/inventory/hosts
-sudo echo " " >> /etc/ansible/inventory/hosts
-sudo echo "[node]" >> /etc/ansible/inventory/hosts
-sudo secho "0.0.0.0 ansible_host=ansible-node1" >> /etc/ansible/inventory/hosts
-sudo secho "0.0.0.1 ansible_host=ansible-node2" >> /etc/ansible/inventory/hosts
-
-ansible-galaxy role search "gitlab"
-ansible-galaxy role install -p ./ activatedinc.gitlab
-ansible-galaxy role install -p ./ geerlingguy.haproxy
-git clone https://github.com/milesbarry/rhel-repo-sync.git
+sudo dnf install -y pip
+pip3 install ansible
 
 ssh-keygen
 ssh-copy-id ansible@ansible-node1
 ssh-copy-id ansible@ansible-node2
+
+sudo mkdir -p /etc/ansible/inventory
+sudo mkdir -p /etc/ansible/playbooks
+sudo mkdir -p /etc/ansible/roles
+sudo chown -R ansible:ansible /etc/ansible
+ansible-config init >> /etc/ansible/ansible.cfg
+
+sudo sed -i 's/inventory=*/inventory=/etc/ansible/inventory/hosts/' /etc/ansible/ansible.cfg
+sudo sed -i 's/^forks=*/forks=50/' /etc/ansible/ansible.cfg
+sudo sed -i 's/^remote_user=*/remote_user=ansible/' /etc/ansible/ansible.cfg
+sudo sed -i 's/^become_user=*/become_user=ansible/' /etc/ansible/ansible.cfg
+
+sudo touch /etc/ansible/inventory/hosts
+sudo bash -c "echo "[control]" >> /etc/ansible/inventory/hosts"
+sudo bash -c "echo "localhost ansible_host=localhost ansible_connection=local" >> /etc/ansible/inventory/hosts"
+sudo bash -c "echo ' ' >> /etc/ansible/inventory/hosts"
+sudo bash -c "echo '[node]' >> /etc/ansible/inventory/hosts"
+sudo bash -c "echo '0.0.0.0 ansible_host=ansible-node1' >> /etc/ansible/inventory/hosts"
+sudo bash -c "echo '0.0.0.1 ansible_host=ansible-node2' >> /etc/ansible/inventory/hosts"
+
+# ansible-galaxy role search "gitlab"
+# ansible-galaxy role install -p ./ activatedinc.gitlab
+# ansible-galaxy role install -p ./ geerlingguy.haproxy
+# git clone https://github.com/milesbarry/rhel-repo-sync.git
