6c2c1af5020d43124be430e7e62fb2272223c052
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2023-10-14 10:09:14 -0500

    updated eks vars

--- Terraform/development/aws/eks/main.tf
+++ Terraform/development/aws/eks/main.tf
@@ -6,10 +6,12 @@ module "eks-use1" {
   source                         = "terraform-aws-modules/eks/aws"
   version                        = "~> 19.0"
   for_each                       = var.clusters_use1
+  aws_auth_roles                 = try(each.value.aws_auth_roles, null)
+  aws_auth_users                 = try(each.value.aws_auth_users, null)
+  aws_auth_accounts              = try(each.value.aws_auth_accounts, null)
   cluster_name                   = each.key
   cluster_version                = each.value.cluster_version
   cluster_endpoint_public_access = each.value.cluster_endpoint_public_access
-
   cluster_addons = {
     coredns = {
       most_recent = each.value.enable_coredns
@@ -21,16 +23,13 @@ module "eks-use1" {
       most_recent = each.value.enable_vpc_cni
     }
   }
-  vpc_id                          = each.value.vpc_id
-  subnet_ids                      = each.value.subnet_ids
   control_plane_subnet_ids        = each.value.control_plane_subnet_ids
+  create_aws_auth_configmap       = each.value.manage_aws_auth_configmap
   eks_managed_node_group_defaults = each.value.eks_managed_node_group_defaults
   eks_managed_node_groups         = each.value.eks_managed_node_groups
   manage_aws_auth_configmap       = each.value.manage_aws_auth_configmap
-  create_aws_auth_configmap       = each.value.manage_aws_auth_configmap
-  aws_auth_roles                  = try(each.value.aws_auth_roles, null)
-  aws_auth_users                  = try(each.value.aws_auth_users, null)
-  aws_auth_accounts               = try(each.value.aws_auth_accounts, null)
+  subnet_ids                      = each.value.subnet_ids
+  vpc_id                          = each.value.vpc_id
   tags                            = try(each.value.tags, null)
   providers = {
     aws = aws.use1
--- Terraform/development/aws/vpc/variables.tf
+++ Terraform/development/aws/vpc/variables.tf
@@ -1,12 +1,13 @@
 variable "networks_use1" {
   default = {
     "skylab" = {
+      azs  = ["us-east-1a", "us-east-1c"]
+      cidr = "192.168.0.0/16"
       # enable_nat_gateway = true
+      map_public_ip_on_launch = true
+      private_subnets         = ["192.168.0.0/24", "192.168.1.0/24"]
+      public_subnets          = ["192.168.10.0/24", "192.168.11.0/24"]
       # single_nat_gateway = true
-      cidr            = "192.168.0.0/16"
-      azs             = ["us-east-1a", "us-east-1c"]
-      private_subnets = ["192.168.0.0/24", "192.168.1.0/24"]
-      public_subnets  = ["192.168.10.0/24", "192.168.11.0/24"]
       tags = {
         environment = "dev"
         node_group  = "small"
