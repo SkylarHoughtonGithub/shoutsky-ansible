d7ae784dfeb9f3fd68b33d2bbe778ef204d3b6fd
SkylarHoughtonGithub <skylarhoughton1996@gmail.com>
2024-01-10 16:02:14 -0600

    removed ssube

--- RHCE8/practice-test/Test2/roles/ssube/build-tools/scripts/tool-git.mk
+++
@@ -1,4 +0,0 @@
-# Git
-git-push: ## push to both gitlab and github
-	$(PREFIX_CMD) git push github ${GIT_BRANCH}
-	$(PREFIX_CMD) git push gitlab ${GIT_BRANCH}
--- RHCE8/practice-test/Test2/roles/ssube/build-tools/terraform/modules/aws/acm/cert/main.tf
+++
@@ -1,4 +0,0 @@
-data "aws_acm_certificate" "acm_cert" {
-  domain    = "${var.cert_domain}"
-  statuses  = ["ISSUED"]
-}
